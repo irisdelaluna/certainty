011f74926786f95f68cb6074411c9ea409b9f0f9
Irys Łuna <dude@irysek.pro>
2025-12-14 23:32:57 +0100

    Implement ordering predicate

--- lib/certainty/order.rb
+++ lib/certainty/order.rb
@@ -8,12 +8,23 @@ module Certainty
 
     # Indicates neither of known `DIRECTIONS`.
     class UnknownDirectionError < ArgumentError
+      def initialize(order)
+        super("Unknown direction: #{order}")
+      end
     end
 
     # Check whether given `list` is in the specified `order`,
     # and if sorted `by` given attribute.
     def self.sorted?(list, order, by: :itself)
-      # TODO: Implement
+      raise UnknownDirectionError, order unless DIRECTIONS.include?(order)
+
+      expected_cmp = order == :desc ? 1 : -1
+
+      list.each_cons(2).all? do |x, y|
+        a, b = [x, y].map { _1.public_send by.to_sym }
+        cmp = a <=> b and cmp.zero? and next true
+        cmp == expected_cmp
+      end
     end
   end
 end

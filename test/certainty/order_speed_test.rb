9711c8c74da7f1663660b2df7ff51edd25578b0a
Irys Łuna <dude@irysek.pro>
2025-12-15 13:47:32 +0100

    Benchmark the sort order predicate

---
+++ test/certainty/order_speed_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Certainty
+  class OrderSpeedTest < PerformanceTest
+    def self.bench_range
+      bench_linear(2, 7_000, 1_000)
+    end
+
+    def bench_mixed
+      samples = make_samples
+
+      # Constant because of the predicate returning early, just as soon
+      # as a sort inconsistency is found.
+      assert_performance_constant do
+        Order.sorted?(samples.shift, :desc)
+      end
+    end
+
+    def bench_sorted
+      samples = make_samples.each(&:sort!)
+
+      # Linear due to looping over the entire list when sorted.
+      assert_performance_linear 0.7 do
+        Order.sorted?(samples.shift, :asc)
+      end
+    end
+
+    private
+
+    def make_samples
+      self.class.bench_range.map do |sample_size|
+        sample_size.times.map { rand((_1 + 1) * 100) }
+      end
+    end
+  end
+end
